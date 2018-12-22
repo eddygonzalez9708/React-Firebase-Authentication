0772948ef6a982d0697a633967f0552db77a6b18
Eddy <edward.gonzalez9708@gmail.com>
2018-12-22 11:30:38 -0500

    Added Routes to the App component and added boilerplate code to any component not being used at the moment.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import Navigation from '../Navigation'
+import Landing from '../Landing'
+import SignUp from '../SignUp'
+import SignIn from '../SignIn'
+import PasswordForget from '../PasswordForget'
+import Home from '../Home'
+import Account from '../Account'
+import Admin from '../Admin'
+
+import { Route } from 'react-router-dom'
+
+import * as ROUTES from '../../constants/routes'
 
 const App = () => (
   <div>
     <Navigation />
+    <hr />
+    <Route exact path={ROUTES.LANDING} component={Landing} />
+    <Route exact path={ROUTES.SIGN_UP} component={SignUp} />
+    <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+    <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+    <Route exact path={ROUTES.HOME} component={Home} />
+    <Route exact path={ROUTES.ACCOUNT} component={Account} />
+    <Route exact path={ROUTES.ADMIN} component={Admin} />
   </div>
 )
 
