77b1091b413841d803565f41db1fb4e60ef875ec
Eddy <edward.gonzalez9708@gmail.com>
2019-01-07 22:37:36 -0500

    Wrote comments in root index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Firebase, { FirebaseContext } from './components/Firebase'
 
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
+/* Relative path to the App component was changed */
+
 import App from './components/App'
-import Firebase, { FirebaseContext } from './components/Firebase'
 
 ReactDOM.render(
+  /*
+  Firebase instance created with the Firebase class and
+  a value prop passed to the React Context. Doing it
+  this way, we can be assured that Firebase is only instantiated
+  once and that it is injected via React's Context API to React's
+  component tree. Now, every component that is interested in using
+  Firebase has access to the Firebase instance with a
+  FirebaseContext.Consumer component.
+  */
+ 
   <FirebaseContext.Provider value={new Firebase()}>
     <Router>
       <App />
